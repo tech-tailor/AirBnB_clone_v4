9bed6455ea147cdc105ca16ded652f380bfcae2f
KIHALAya <kihalaya00@gmail.com>
2024-03-03 17:55:12 +0000

    show and hide reveiws

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-""" Starts a Flash Web Application """
+tarts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -19,7 +18,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/100-hbnb/', strict_slashes=False)
+@app.route('/101-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -36,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('101-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
